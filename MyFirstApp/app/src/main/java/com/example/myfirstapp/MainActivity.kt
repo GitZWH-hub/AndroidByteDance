91174990a545028aa65ac32e21c1bd0ec6965d20
张卫华 <21212010055@m.fudan.edu.cn>
2021-10-20 16:43:03 +0800

    添加注释：姓名、日期

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/*
+*   张卫华 2021-10-20
+*
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
